df7dc1e17a9b1373496c5612a13072967b312b32
RobertLBusby <42418427+RobertLBusby@users.noreply.github.com>
2019-03-12 09:21:52 -0400

    Add files via upload

--- SuperSecureMaster.bat
+++ SuperSecureMaster.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-title Folder Private
+title Folder Private (c) 2019 Robert Busby
 if EXIST SSS/SSSA.vbs goto READY
 if EXIST SSS/SSSA1.vbs goto READY
 if EXIST SSS/SSSA2.vbs goto READY
@@ -8,9 +8,11 @@ if NOT EXIST SSS/SSSA.vbs goto End
 if NOT EXIST SSS/SSSA1.vbs goto End
 if NOT EXIST SSS/SSSA2.vbs goto End
 if NOT EXIST SSS/SSSA3.bat goto End
+
 :READY
 if EXIST "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" goto UNLOCK
-if NOT EXIST Private goto MDLOCKER
+if NOT EXIST Private goto SSScreate
+
 :CONFIRM
 echo Are you sure you want to lock the folder(Y/N)
 set/p "cho=>"
@@ -20,11 +22,13 @@ if %cho%==n goto END
 if %cho%==N goto END
 echo Invalid choice.
 goto CONFIRM
+
 :LOCK
 ren Private "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 echo Folder locked
 goto End
+
 :UNLOCK
 echo Enter password to unlock folder
 set/p "pass=>"
@@ -33,6 +37,7 @@ attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
 ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" Private
 echo Folder Unlocked successfully
 goto End
+
 :FAIL
 echo Invalid password
 start SSS/SSSA.vbs
@@ -41,7 +46,8 @@ start SSS/SSSA2.vbs
 sleep 30
 start SSS/SSSA3.bat
 goto end
-:MDLOCKER
+
+:SSScreate
 md Private
 echo Folder created successfully
 goto End
